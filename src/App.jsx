d4279774828b5ddf47db64448df39847372eae43
Hacene010 <hamrounehacene06@gmail.com>
2021-12-19 19:21:47 +0100

    Set up the routes that call different components and the global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import SApp from './Style';
+import MovieList from './components/MovieList/MovieList';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import CreateMovie from './components/CreateMovie/CreateMovie';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <SApp className='App'>
-      <h1>Starting project</h1>
+    <SApp className="App">
+      <Header />
+      <Routes>
+        <Route path="/createMovie" element={<CreateMovie />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/" element={<MovieList />} />
+      </Routes>
     </SApp>
   );
 }
--- src/Style.jsx
+++ src/Style.jsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-const SApp = styled.div``;
+const SApp = styled.div`
+  text-align: center;
+  .link {
+    color: #000;
+    text-decoration: none;
+  }
+`;
 
 export default SApp;
