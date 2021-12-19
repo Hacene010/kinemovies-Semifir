d4fdd4887a68bed1ae5c831d71841a2aed5df86c
Hacene010 <hamrounehacene06@gmail.com>
2021-12-19 19:27:43 +0100

    Renamed the AddMovie component to CreateMovie

--- src/components/AddMovie/AddMovie.jsx
+++
@@ -1,7 +0,0 @@
-export default function AddMovie() {
-  return (
-    <div>
-      <h2>Adding Movies</h2>
-    </div>
-  );
-}
--- src/components/AddMovie/Style.jsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-const SAddMovie = styled.div``;
-
-export default SAddMovie;
