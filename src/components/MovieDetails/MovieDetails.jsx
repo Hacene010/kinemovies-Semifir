135523a5decf8709945aca0f075ec3c62e3dbc15
Hacene010 <hamrounehacene06@gmail.com>
2021-12-19 19:26:23 +0100

    Created the MovieDetails component and its style

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,64 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import SMovieDetails from './Style';
+
 export default function MovieDetails() {
+  const [movie, setMovie] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/movies/${id}`)
+      .then(({ data }) => {
+        const { actors, releaseDate } = data;
+        const newActors = actors.split(',');
+        const newFormat = releaseDate.split('-').reverse().join('-');
+        setMovie({ ...data, actors: newActors, releaseDate: newFormat });
+      })
+      .catch((err) => `Erreur : ${err}`);
+  }, [id]);
+
   return (
-    <div>
-      <h2>Details of one movie</h2>
-    </div>
+    <SMovieDetails>
+      <h2 className="title">{movie.title}</h2>
+      <div className="container">
+        <div className="attach">
+          <img src={movie.attach} alt="movieAttach" />
+        </div>
+        <div className="infosContainer">
+          <h3>
+            Synopsis <span>{movie.synopsis}</span>
+          </h3>
+
+          <div className="directorRelease">
+            <h3>
+              Date de sortie <span>{movie.releaseDate}</span>
+            </h3>
+            <h3>
+              Réalisateur(s) <span>{movie.director}</span>
+            </h3>
+          </div>
+          <div className="durationDistributor">
+            <h3>
+              Durée <span>{movie.duration} min</span>
+            </h3>
+            <h3>
+              Disponible sur <span>{movie.distributer}</span>
+            </h3>
+          </div>
+
+          <h3>
+            Acteurs
+            <span>
+              {movie?.actors?.map((actor) => {
+                return <li key={actor}>{actor}</li>;
+              })}
+            </span>
+          </h3>
+        </div>
+      </div>
+    </SMovieDetails>
   );
 }
--- src/components/MovieDetails/Style.jsx
+++ src/components/MovieDetails/Style.jsx
@@ -1,5 +1,76 @@
 import styled from 'styled-components';
 
-const SMovieDetails = styled.div``;
+const SMovieDetails = styled.article`
+  font-family: var(--hFont);
+  padding: 1rem;
+  background: rgb(121, 0, 0);
+  background: radial-gradient(
+    circle,
+    rgba(121, 0, 0, 1) 0%,
+    rgba(0, 0, 0, 1) 78%
+  );
+
+  .title {
+    color: var(--hColor);
+    font-size: 2.5rem;
+  }
+
+  .container {
+    width: 60vw;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+
+    .attach {
+      width: 100%;
+
+      img {
+        width: 75%;
+        border-radius: 8px;
+        box-shadow: 0px 0px 20px 1px rgb(129, 61, 61);
+      }
+    }
+
+    .infosContainer {
+      margin: 2rem;
+      width: 70vw;
+
+      h3 {
+        display: flex;
+        flex-direction: column;
+        color: var(--hColor);
+        font-size: 2rem;
+
+        span {
+          padding: 0.5rem;
+          color: var(--pColor);
+          font-family: var(--pFont);
+          font-size: 1.2rem;
+        }
+      }
+
+      li {
+        margin: 0.3rem;
+        list-style-type: none;
+      }
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    padding: 0;
+
+    .container {
+      width: 90vw;
+      padding: 0;
+      flex-direction: column;
+
+      .infosContainer {
+        margin: 0;
+      }
+    }
+  }
+`;
 
 export default SMovieDetails;
